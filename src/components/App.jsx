d2fac3960e3c07fe189465a98b2f5db9991707ee
Alena-007 <alena.tv@ukr.net>
2022-11-29 16:03:51 +0200

    add bug fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,38 +10,25 @@ export const App = () => {
   const [status, setStatus] = useState('all');
   const [filteredTodos, setFilteredTodos] = useState([]);
 
-  const filterHandler = () => {
-    switch (status) {
-      case 'completed':
-        setFilteredTodos(todos.filter(todo => todo.completed === true));
-        break;
-      case 'uncompleted':
-        setFilteredTodos(todos.filter(todo => todo.completed === false));
-        break;
-      default:
-        setFilteredTodos(todos);
-        break;
-    }
-  };
-
-  const saveLocalTodos = () => {
-    localStorage.setItem('todos', JSON.stringify(todos));
-  };
-
-  // const getLocalTodos = () => {
-  //   if (localStorage.getItem('todos') === null) {
-  //     localStorage.setItem('todos', JSON.stringify([]));
-  //   } else {
-  //     let localTodo = JSON.parse(localStorage.getItem('todos'));
-  //     setTodos(localTodo);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   getLocalTodos();
-  // }, []);
-
   useEffect(() => {
+    const filterHandler = () => {
+      switch (status) {
+        case 'completed':
+          setFilteredTodos(todos.filter(todo => todo.completed === true));
+          break;
+        case 'uncompleted':
+          setFilteredTodos(todos.filter(todo => todo.completed === false));
+          break;
+        default:
+          setFilteredTodos(todos);
+          break;
+      }
+    };
+
+    const saveLocalTodos = () => {
+      localStorage.setItem('todos', JSON.stringify(todos));
+    };
+
     filterHandler();
     saveLocalTodos();
   }, [todos, status]);
